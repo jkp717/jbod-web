7f6bd7965d5323858c3dd51f4e4642f0c6266e9d
ProfoundlyItchy <joshkprice717@gmail.com>
2023-04-03 13:28:12 -0500

    decoding base64 env var

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+#
+# Flask Configurations
+#
+
 import os
+import base64
 from dotenv import load_dotenv
 
 
@@ -9,7 +14,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config:
     SCHEDULER_API_ENABLED = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SECRET_KEY_SALT = os.environ.get('SECRET_KEY_SALT').encode()
+    SECRET_KEY_SALT = base64.b64decode(bytes(os.environ.get('SECRET_KEY_SALT'), "utf-8"))
     FLASK_ADMIN_FLUID_LAYOUT = True
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'instance', 'ipmi.db')}"
     SCHEDULER_JOBS = []  # APScheduler Jobs
