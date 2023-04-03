1acba484d064a3df12bc415d20f668baee82f4c7
ProfoundlyItchy <joshkprice717@gmail.com>
2023-04-03 13:26:12 -0500

    Store var as string using base64 encoding

--- scripts/setup_env.py
+++ scripts/setup_env.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python3
+
 import os
+import base64
 from cryptography.fernet import Fernet
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 
 if __name__ == '__main__':
     if not os.path.exists(os.path.join(basedir, '.env')):
         with open(os.path.join(basedir, '.env'), 'w') as f:
-            f.write(f"SECRET_KEY={Fernet.generate_key()}\n")
-            f.write(f"SECRET_KEY_SALT={os.urandom(16)}\n")
+            f.write(f"SECRET_KEY={Fernet.generate_key().decode()}\n")
+            f.write(f"SECRET_KEY_SALT={base64.b64encode(os.urandom(16)).decode()}\n")
